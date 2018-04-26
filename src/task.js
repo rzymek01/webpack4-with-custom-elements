1bacb0670215a5ecd77e1f89bd6113edaa9e1f60
Maciej Rzymski <mrzymski@atlassian.com>
2018-04-26 14:22:25 +0200

    06-example: skatejs + customised button element

--- src/task.js
+++ src/task.js
@@ -1,59 +1,38 @@
+import {withComponent, props, define} from 'skatejs';
+
+
 // base class to extend, same trick as before
 class HTMLCustomElement extends HTMLButtonElement {
     constructor(_) { return (_ = super(_)).init(), _; }
     init() { /* override as you like */ }
 }
 
-export default class Task extends HTMLCustomElement {
-    static get observedAttributes() {
-        return ['done'];
+export default class Task extends withComponent(class extends HTMLCustomElement {}) {
+    static is = 'app-task';
+    static props = {
+        done: {...props.boolean, ...{attribute: true}},
+    };
+
+    // do not use ShadowDOM
+    get renderRoot() {
+        return this;
     }
 
     init() {
         console.log('app-task constructor');
-
-        this.name = '#number';
 
         this.addEventListener("click", () => {
             this.done = !this.done;
         });
     }
 
-    connectedCallback() {
+    connected() {
         console.log('app-task connectedCallback');
-
-        this.render();
-    }
-
-    disconnectedCallback() {
-        console.log('app-task disconnectedCallback');
-    }
-
-    attributeChangedCallback(attrName, oldVal, newVal) {
-        console.log('app-task attributeChangedCallback', attrName, oldVal, newVal);
-
-        this.render();
-    }
-
-    adoptedCallback() {
-        console.log('app-task adoptedCallback');
     }
 
     render() {
         console.log('app-task render; done =', this.done);
-        this.innerHTML = `${this.name}: ${this.done ? 'done' : 'todo'}`;
+        return `${this.done ? 'done' : 'todo'}`;
     }
-
-    get done() {
-        return this.hasAttribute('done');
-    }
-
-    set done(val) {
-        if (val) {
-            this.setAttribute('done', '');
-        } else {
-            this.removeAttribute('done');
-        }
-    }
-};
+}
 window.customElements.define('app-task', Task, {extends: 'button'});
